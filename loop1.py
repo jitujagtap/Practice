fae49ae13d41fad0a8dad0467a9d3606f444a20b
jitujagtap <56694040+jitujagtap@users.noreply.github.com>
2022-08-20 15:35:57 +0530

    Add files via upload

---
+++ loop1.py
@@ -0,0 +1,17 @@
+for i in range(1,11):
+    print(i,end=' ')
+####
+print()
+for j in range(11,0,-1):
+    print(j,end=' ')
+
+for k in range(100,10,-1):
+    print(k)
+#Table of 2
+for k in range(2,21,+2):
+    print(k)
+#factorial of 5
+fact=1
+for k in range(1,6,+1):
+    fact=fact*k
+print("Factorial of number ",fact)
