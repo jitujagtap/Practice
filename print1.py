3dd070ea73173badab19df2f7973e446ac4a7577
jitujagtap <56694040+jitujagtap@users.noreply.github.com>
2022-08-20 23:36:59 +0530

    Add files via upload

---
+++ print1.py
@@ -0,0 +1,13 @@
+print("Welcome to javaTpoint.")
+
+a = 10
+# Two objects are passed in print() function
+print("a =", a)
+
+b = a
+# Three objects are passed in print function
+print('a =', a, '= b')
+
+a=int(input("Enter first Number : "))
+b=int(input("Enter second Number : "))
+print(a+b)
