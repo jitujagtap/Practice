f9c1ce07b2217532bfcce12a6f13529c67920aa2
jitujagtap <56694040+jitujagtap@users.noreply.github.com>
2022-08-20 16:43:18 +0530

    Add files via upload

---
+++ Exception_Handling1.py
@@ -0,0 +1,6 @@
+while True:
+    try:
+        num1 = int(input("Enter Number: "))
+        print("The entered Number is:", num1)
+    except:
+        print("Please enter valid number, this is not a valid number.")
