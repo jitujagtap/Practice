355b2c5b6d19d3b97fc5490cfeef9db3a30bc0fe
jitujagtap <56694040+jitujagtap@users.noreply.github.com>
2022-08-21 22:29:55 +0530

    Delete Exception_Handling1.py

--- Exception_Handling1.py
+++
@@ -1,6 +0,0 @@
-while True:
-    try:
-        num1 = int(input("Enter Number: "))
-        print("The entered Number is:", num1)
-    except:
-        print("Please enter valid number, this is not a valid number.")
